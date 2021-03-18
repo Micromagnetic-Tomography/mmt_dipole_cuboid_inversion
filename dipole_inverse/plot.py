811ec6ec97a7f686722e73a5dee1914f33e9b10e
davidcortesortuno <david.cortes.o@gmail.com>
2021-03-18 16:29:24 +0100

    Refactored plot functions to a more modular approach

--- dipole_inverse/plot.py
+++ dipole_inverse/plot.py
@@ -1,89 +1,45 @@
 import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
 import matplotlib as mpl
 import numpy as np
 from shapely.geometry import Polygon
 from descartes import PolygonPatch
 from shapely.ops import cascaded_union
+import grain_geometry_tools as ggt
 
 
-def plot_contour(DipoleIns, ax, tol=1e-7):
-    """ Using an instance of the Dipole class, plots contour of grains at a
-    given matplotlib axis object
-
-    Parameters
-    ----------
-    DipoleIns
-        An instance of the Dipole class
-    ax
-        Matplotlib axis.
-    tol
-        Tolerance is used to enable merging cuboids of one grain.
+def set_grain_geometries(DipoleIns, tol=1e-7):
+    """
+    Generates multiple arrays with grain geometries and apply them to the
+    DipoleIns instance
     """
 
-    counter = 0
-    grainnr = 1
-    # plot grains with number
-    for i in range(DipoleIns.cuboids.shape[0]):
-        xs, ys = DipoleIns.cuboids[i, 0:2]
-        dx, dy = DipoleIns.cuboids[i, 3:5]
+    DipoleIns.cuboid_idxs = DipoleIns.cuboids[:, 6].astype(np.int32)
+    (DipoleIns.cuboid_idxs_unique,
+     DipoleIns.cuboid_idxs_counts) = np.unique(DipoleIns.cuboid_idxs,
+                                               return_counts=True)
 
-        # Create overlapping polygon
-        polygon = Polygon([(xs - dx - tol, ys - dy - tol),
-                           (xs - dx - tol, ys + dy + tol),
-                           (xs + dx + tol, ys + dy + tol),
-                           (xs + dx + tol, ys - dy - tol)])
-
-        # If continuing with same grain
-        if DipoleIns.cuboids[i, 6] == grainnr:
-            if counter == 0:  # No grain created before
-                united_polygons = polygon
-                counter += 1
-            else:  # If a polygon has been made before
-                # Merge polygons into one
-                united_polygons = cascaded_union([polygon,
-                                                  united_polygons])
-                counter += 1
-
-        # Last particle
-        if i == DipoleIns.cuboids.shape[0] - 1:
-            united_polygons = cascaded_union([polygon,
-                                              united_polygons])
-            mean = united_polygons.representative_point().wkt
-            mean = np.matrix(mean[7:-1])
-            ax.add_patch(PolygonPatch(united_polygons,
-                                      facecolor="None",
-                                      edgecolor='black'))
-            ax.text(mean[0, 0], mean[0, 1], int(grainnr),
-                    fontsize=20,
-                    horizontalalignment="center",
-                    verticalalignment="center")
-
-            # If new grain is started
-        if DipoleIns.cuboids[i, 6] != grainnr:
-            ax.add_patch(PolygonPatch(united_polygons,
-                                      facecolor="None",
-                                      edgecolor='black'))
-            # Get center united polygon to plot grain number there
-            mean = united_polygons.representative_point().wkt
-            mean = np.matrix(mean[7:-1])
-
-            # Get grain number plotted with corresponding grain
-            ax.text(mean[0, 0], mean[0, 1], int(grainnr),
-                    fontsize=20,
-                    horizontalalignment="center",
-                    verticalalignment="center")
-            grainnr = int(DipoleIns.cuboids[i, 6])  # New grain number
-            del united_polygons  # Delete old assembled polygon
-            counter = 0  # Reset counter
-            united_polygons = polygon  # Start with new polygon
-            counter += 1
-    return ax
+    # Generate vertexes to plot the grains
+    DipoleIns.grain_vertices = ggt.generate_grain_vertices(DipoleIns.cuboids)
+    (DipoleIns.grain_geoms,
+     DipoleIns.grain_geoms_coords,
+     DipoleIns.grain_centroids) = ggt.generate_grain_geometries(
+        DipoleIns.cuboids, DipoleIns.grain_vertices,
+        DipoleIns.cuboid_idxs, DipoleIns.cuboid_idxs_unique,
+        polygon_buffer=tol,
+        generate_centroids=True)
 
 
-def plot_magnetization(DipoleIns, ax, ax2=None,
-                       colormap='coolwarm', tol=1e-7):
+def plot_grain_boundaries(DipoleIns, ax,
+                          grain_labels=True,
+                          tol=1e-7):
     """
-    Plots the magnetization of grains with colorscale on a Matplotlib axis
+    Plots the grain boundaries viewed from a bird eye perspective at the
+    z-axis
+
+    Note
+    ----
+    Requires set_grain_geometries to be applied to DipoleIns beforehand
 
     Parameters
     ----------
@@ -91,144 +47,150 @@ def plot_magnetization(DipoleIns, ax, ax2=None,
         An instance of the Dipole class
     ax
         Matplotlib axis
-    ax2
-        Optional axis for the colorbar
+    tol
+        Tolerance used to enable merging cuboids of one grain
+    """
+    grain_geoms_coords = DipoleIns.grain_geoms_coords
+
+    # Draw grain outlines -----------------------------------------------------
+    for i, pg_key in enumerate(grain_geoms_coords):
+        # We can use i to colour the grain
+        col = PolyCollection([grain_geoms_coords[pg_key]],
+                             # fc=cmap(i + 1),
+                             fc=(0, 0, 0, 0),
+                             ec=(0, 0, 0, 1),
+                             )
+        ax.add_collection(col)
+
+    if grain_labels:
+        grain_cs = DipoleIns.grain_centroids
+        for grain_idx in grain_cs:
+            ax.text(grain_cs[grain_idx][0], grain_cs[grain_idx][1],
+                    grain_idx,
+                    # fontsize=20,
+                    ha="center",
+                    va="center")
+
+    ax.autoscale()
+
+    return None
+
+
+def plot_magnetization_on_grains(DipoleIns, ax,
+                                 mag_log_scale=True,
+                                 plot_labels=True,
+                                 colormap='coolwarm',
+                                 tol=1e-7):
+    """
+    Plots the magnetization of grains with colorscale on a Matplotlib axis
+
+    Note
+    ----
+    Requires set_grain_geometries to be applied to DipoleIns beforehand
+
+    Parameters
+    ----------
+    DipoleIns
+        An instance of the Dipole class
+    ax
+        Matplotlib axis
     colormap
-        default colormap is coolwarm
+        Colormap of the magnetization
     tol
         Tolerance used to enable merging cuboids of one grain
     """
 
-    counter = 0  # Keeps track of amount of cuboids per grain
-    counter2 = 0  # Keeps track of amount of grains
-    cvalmax = 0
-    grainnr = 1
+    mag = np.linalg.norm(DipoleIns.Mag.reshape(-1, 3), axis=1)
+    # cuboid_idxs = DipoleIns.cuboid_idxs
+    cuboid_idxs_unique, cuboid_idxs_counts = (DipoleIns.cuboid_idxs_unique,
+                                              DipoleIns.cuboid_idxs_counts)
 
-    # Get minimum/maximum magnetization to make proper colorscale
-    cvalmin = np.sqrt(DipoleIns.Mag[0] ** 2 + DipoleIns.Mag[1] ** 2
-                      + DipoleIns.Mag[2] ** 2)
-    partmin = 1
-    # If more than one grain
-    if DipoleIns.cuboids.shape[0] > 1:
-        for i in range(DipoleIns.Npart):
-            cvalnew = np.sqrt(np.power(DipoleIns.Mag[3 * i], 2)
-                              + np.power(DipoleIns.Mag[3 * i + 1], 2)
-                              + np.power(DipoleIns.Mag[3 * i + 2], 2))
-            if cvalnew > cvalmax:
-                cvalmax = cvalnew
-                partmax = i + 1
-            if cvalmin > cvalnew:
-                cvalmin = cvalnew
-                partmin = i + 1
-        print(f"Minimum is {cvalmin} at particle {partmin}."
-              f" Maximum is {cvalmax} at particle {partmax}.")
-        # get colormap
-        coolwarm = mpl.cm.get_cmap(colormap,
-                                   int((np.log10(cvalmax)
-                                        - np.log10(cvalmin))
-                                       * 1000))
-    else:
-        cvalmax = cvalmin + 100
-        cvalmin = cvalmin - 100
-        print('Only one grain')
-        coolwarm = mpl.cm.get_cmap(colormap, 200)
+    # Generate vertexes to plot the grains
+    grain_vertices = DipoleIns.grain_vertices
+    grain_geoms_coords, grain_cs = (DipoleIns.grain_geoms_coords,
+                                    DipoleIns.grain_centroids)
 
-    # Plot grains now with number
-    for i in range(DipoleIns.cuboids.shape[0]):
-        xs, ys = DipoleIns.cuboids[i, 0:2]
-        dx, dy = DipoleIns.cuboids[i, 3:5]
+    # Draw grain shapes and color according to magnetization
+    col = PolyCollection(grain_vertices)
+    mag_per_cuboid = np.repeat(mag, cuboid_idxs_counts)
+    if mag_log_scale:
+        mag_per_cuboid = np.log(mag_per_cuboid)
+    col.set(array=mag_per_cuboid, cmap='coolwarm',
+            # clim=(1e2, 1e8)
+            )
+    ax.add_collection(col)
 
-        # Create polygon
-        polygon = Polygon([(xs - dx - tol, ys - dy - tol),
-                           (xs - dx - tol, ys + dy + tol),
-                           (xs + dx + tol, ys + dy + tol),
-                           (xs + dx + tol, ys - dy - tol)])
+    if plot_labels:
+        for grain_idx in grain_cs:
+            ax.text(grain_cs[grain_idx][0], grain_cs[grain_idx][1],
+                    grain_idx,
+                    # fontsize=20,
+                    ha="center",
+                    va="center")
 
-        # If continuing with same grain
-        if DipoleIns.cuboids[i, 6] == grainnr:
-            # If cuboid part of same grain
-            if counter == 0:  # If no polygon has been made before
-                united_polygons = polygon
-                counter += 1
-            else:  # If a polygon has been made before
-                united_polygons = cascaded_union([polygon,
-                                                  united_polygons])
-                # Merge polygons into one
-                counter += 1
+    ax.autoscale()
 
-        # If new grains is started
-        if DipoleIns.cuboids[i, 6] != grainnr:
-            cvalue = np.sqrt(np.power(DipoleIns.Mag[3 * counter2], 2)
-                             + np.power(DipoleIns.Mag[
-                                 3 * counter2 + 1], 2)
-                             + np.power(DipoleIns.Mag[
-                                 3 * counter2 + 2], 2))
+    return None
 
-            ax.add_patch(PolygonPatch(united_polygons,
-                                      facecolor=coolwarm(int((
-                                          np.log10(cvalue)
-                                          - np.log10(cvalmin))
-                                          * 1000)),
-                                      edgecolor='black'))
 
-            mean = united_polygons.representative_point().wkt
-            mean = np.matrix(mean[7:-1])
-            # Get grain numbers with grains
-            ax.text(mean[0, 0], mean[0, 1], int(grainnr),
-                    fontsize=20,
-                    horizontalalignment="center",
-                    verticalalignment="center")
-            grainnr = int(DipoleIns.cuboids[i, 6])  # Grain number
-            del united_polygons  # Delete old assembled polygon
-            counter = 0  # Reset counter
-            united_polygons = polygon  # Start with new polygon
-            counter += 1
-            counter2 += 1
+def plot_scan_field(DipoleIns,
+                    ax,
+                    imshow_args=dict(cmap='magma')):
+    """
+    """
 
-        # If last particle is reached
-        if i == DipoleIns.cuboids.shape[0] - 1:
-            cvalue = np.sqrt(np.power(DipoleIns.Mag[3 * counter2], 2)
-                             + np.power(DipoleIns.Mag[
-                                 3 * counter2 + 1], 2)
-                             + np.power(DipoleIns.Mag[
-                                 3 * counter2 + 2], 2))
-            united_polygons = cascaded_union([polygon,
-                                              united_polygons])
-            mean = united_polygons.representative_point().wkt
-            mean = np.matrix(mean[7:-1])
+    dx = DipoleIns.QDM_domain_deltax * 0.5
+    dy = DipoleIns.QDM_domain_deltay * 0.5
 
-            # If only one grain in total, go to else
-            if DipoleIns.cuboids.shape[0] > 1:
-                ax.add_patch(PolygonPatch(united_polygons,
-                                          facecolor=coolwarm(int((
-                                              np.log10(cvalue)
-                                              - np.log10(cvalmin))
-                                              * 1000)),
-                                          edgecolor='black'))
-                norm = mpl.colors.LogNorm(vmin=cvalmin,
-                                          vmax=cvalmax)
+    im = ax.imshow(DipoleIns.QDM_matrix, origin='lower',
+                   extent=[DipoleIns.QDM_domain[0, 0] - dx,
+                           DipoleIns.QDM_domain[1, 0] + dx,
+                           DipoleIns.QDM_domain[0, 1] - dy,
+                           DipoleIns.QDM_domain[1, 1] + dy],
+                   **imshow_args)
 
-            else:
-                ax.add_patch(PolygonPatch(united_polygons,
-                                          facecolor=coolwarm(int(
-                                              cvalue - cvalmin)),
-                                          edgecolor='black'))
-                norm = mpl.colors.Normalize(vmin=cvalmin,
-                                            vmax=cvalmax)
+    return im
 
-            ax.text(mean[0, 0], mean[0, 1], int(grainnr),
-                    fontsize=20,
-                    horizontalalignment="center",
-                    verticalalignment="center")
 
-            if ax2 is not None:
-                cb1 = mpl.colorbar.ColorbarBase(
-                    ax2, cmap=coolwarm, norm=norm,
-                    orientation='horizontal')
-                cb1.set_label('M (A/m)')
-                return ax, ax2
-            else:
-                return ax
+def plot_inversion_field(DipoleIns,
+                         ax,
+                         imshow_args=dict(cmap='magma')):
+    """
+    """
+
+    dx = DipoleIns.QDM_domain_deltax * 0.5
+    dy = DipoleIns.QDM_domain_deltay * 0.5
+
+    inv_field = (DipoleIns.Forward_G @ DipoleIns.Mag).reshape(DipoleIns.Ny, -1)
+    im = ax.imshow(inv_field, origin='lower',
+                   extent=[DipoleIns.QDM_domain[0, 0] - dx,
+                           DipoleIns.QDM_domain[1, 0] + dx,
+                           DipoleIns.QDM_domain[0, 1] - dy,
+                           DipoleIns.QDM_domain[1, 1] + dy],
+                   **imshow_args)
+
+    return im
+
+
+def plot_residual(DipoleIns,
+                  ax,
+                  imshow_args=dict(cmap='magma')):
+    """
+    """
+
+    dx = DipoleIns.QDM_domain_deltax * 0.5
+    dy = DipoleIns.QDM_domain_deltay * 0.5
+    Forward_field = (DipoleIns.Forward_G @ DipoleIns.Mag) / DipoleIns.QDM_area
+    Forward_field.shape = (DipoleIns.Ny, DipoleIns.Nx)
+    res = Forward_field - DipoleIns.QDM_matrix / DipoleIns.QDM_area,
+    im = ax.imshow(res, origin='lower',
+                   extent=[DipoleIns.QDM_domain[0, 0] - dx,
+                           DipoleIns.QDM_domain[1, 0] + dx,
+                           DipoleIns.QDM_domain[0, 1] - dy,
+                           DipoleIns.QDM_domain[1, 1] + dy],
+                   **imshow_args)
+
+    return im
 
 
 # TODO: this function requires to be split. Options need to be more general
