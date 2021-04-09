5928b983a603a8f4f7f8425852cee58b67cb55e8
davidcortesortuno <david.cortes.o@gmail.com>
2021-04-09 17:39:00 +0200

    Added test for a single dipole

---
+++ test/test_single_dipole.py
@@ -0,0 +1,56 @@
+from dipole_inverse_tools import set_max_num_threads
+import numpy as np
+from pathlib import Path
+import time
+import matplotlib.pyplot as plt
+set_max_num_threads(4)
+import dipole_inverse as dpinv  # noqa: E402
+
+
+def test_dipole_class():
+
+    QDMfile = './single_dipole_depth_30_Bzgrid.txt'
+    cuboidfile = './single_dipole_depth_30_cuboids.txt'
+
+    # size of QDM domain
+    QDM_domain = np.array([[0, 0], [40., 40.]]) * 1e-6
+    # Spacing between two QDM sample points
+    QDM_spacing = 2e-6
+    # half length of QDM sensor
+    QDM_deltax = 1e-6
+    # half width of QDM sensor
+    QDM_deltay = 1e-6
+    # area of QDM sensor -> necessary? --> use deltax * deltay
+    QDM_area = 4e-12
+    # thickness of sample -> Unnecessary
+    sample_height = 30e-6
+    # distance between QDM and top sample
+    scan_height = 2e-6
+
+    dip_inversion = dpinv.Dipole(
+        QDMfile, cuboidfile, QDM_domain, QDM_spacing,
+        QDM_deltax, QDM_deltay, QDM_area, sample_height, scan_height)
+
+    dip_inversion.read_files()
+
+    assert(dip_inversion.QDM_matrix.shape[0]) == 21
+    assert(dip_inversion.QDM_matrix.shape[1]) == 21
+
+    print('Testing Numba pop matrix')
+    dip_inversion.prepare_matrix(method='numba', verbose=True)
+    FG_copy = np.copy(dip_inversion.Forward_G)
+
+    print('Comparing Cython pop matrix to Numba code')
+    dip_inversion.prepare_matrix(method='cython', verbose=True)
+    for j, i in [(5, 1), (100, 0), (195, 2), (368, 1)]:
+        assert abs(FG_copy[j, i] - dip_inversion.Forward_G[j, i]) < 1e-8
+
+    dip_inversion.calculate_inverse(method='scipy_pinv2', rcond=1e-25)
+
+    Ms = 4.8e5
+    # Check relative error is less than 1% = 0.01
+    assert abs(dip_inversion.Mag[1] - Ms) / Ms < 1e-2
+
+
+if __name__ == "__main__":
+    test_dipole_class()
