1401746e803d5e6adaae6102d11b9c34747e6de7
davidcortesortuno <david.cortes.o@gmail.com>
2022-01-13 21:02:16 +0100

    Updated test to account for cuda lib

--- test/test_single_dipole.py
+++ test/test_single_dipole.py
@@ -5,6 +5,12 @@ from pathlib import Path
 import time
 import matplotlib.pyplot as plt
 import dipole_inverse as dpinv  # noqa: E402
+try:
+    # the cuda populate_matrix function
+    from dipole_inverse.cython_cuda_lib import pop_matrix_cudalib
+    HASCUDA = True
+except ImportError:
+    HASCUDA = False
 
 
 def test_dipole_class():
@@ -28,10 +34,10 @@ def test_dipole_class():
     scan_height = 2e-6
 
     dip_inversion = dpinv.Dipole(
-        QDMfile, cuboidfile, QDM_domain, QDM_spacing,
+        QDM_domain, QDM_spacing,
         QDM_deltax, QDM_deltay, QDM_area, sample_height, scan_height)
 
-    dip_inversion.read_files()
+    dip_inversion.read_files(QDMfile, cuboidfile, cuboid_scaling_factor=1e-6)
 
     assert(dip_inversion.QDM_matrix.shape[0]) == 21
     assert(dip_inversion.QDM_matrix.shape[1]) == 21
@@ -40,8 +46,8 @@ def test_dipole_class():
     dip_inversion.prepare_matrix(method='numba', verbose=True)
     FG_copy = np.copy(dip_inversion.Forward_G)
 
-    print('Comparing Cython pop matrix to Numba code')
-    dip_inversion.prepare_matrix(method='cuda', verbose=True)
+    print('\nComparing Cython pop matrix to Numba code')
+    dip_inversion.prepare_matrix(method='cython', verbose=False)
     for j, i in [(5, 1), (100, 0), (195, 2), (368, 1)]:
         assert abs(FG_copy[j, i] - dip_inversion.Forward_G[j, i]) < 1e-8
     print(dip_inversion.Forward_G[j, i])
@@ -51,6 +57,19 @@ def test_dipole_class():
     Ms = 4.8e5
     # Check relative error is less than 1% = 0.01
     assert abs(dip_inversion.Mag[1] - Ms) / Ms < 1e-2
+
+    if HASCUDA:
+        print('\nComparing NVIDIA cuda pop matrix to Numba code')
+        dip_inversion.prepare_matrix(method='cuda', verbose=False)
+        for j, i in [(5, 1), (100, 0), (195, 2), (368, 1)]:
+            assert abs(FG_copy[j, i] - dip_inversion.Forward_G[j, i]) < 1e-8
+        print(dip_inversion.Forward_G[j, i])
+
+        dip_inversion.calculate_inverse(method='scipy_pinv2', rcond=1e-25)
+
+        Ms = 4.8e5
+        # Check relative error is less than 1% = 0.01
+        assert abs(dip_inversion.Mag[1] - Ms) / Ms < 1e-2
 
 
 if __name__ == "__main__":
